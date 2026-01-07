6508a08d7a1f9f00a3d594c4cdd03f67cb3578c8
audricfullhardt <fullhardtaudric@gmail.com>
2026-01-08 00:00:37 +0100

    feat(background): add blured blobs

--- components/background/DotBackground.tsx
+++ components/background/DotBackground.tsx
@@ -4,7 +4,8 @@ import DotGrid from '../ui/DotGrid';
 
 export default function DotBackground() {
   return (
-    <div className="fixed inset-0 -z-10">
+    <div className="fixed inset-0 -z-10 overflow-hidden">
+      {/* DotGrid */}
       <DotGrid
         dotSize={6}
         gap={15}
@@ -14,6 +15,39 @@ export default function DotBackground() {
         shockRadius={80}
         shockStrength={10}
         style={{ width: '100%', height: '100%' }}
+      />
+      <div
+        className="absolute rounded-full opacity-70"
+        style={{
+          width: '600px',
+          height: '200px',
+          background: '#0F766A',
+          filter: 'blur(100px)',
+          top: '-5%',
+          left: '0%',
+        }}
+      />
+      <div
+        className="absolute rounded-full opacity-60"
+        style={{
+          width: '500px',
+          height: '500px',
+          background: '#11552E',
+          filter: 'blur(150px)',
+          top: '50%',
+          left: '60%',
+        }}
+      />
+      <div
+        className="absolute rounded-full opacity-50"
+        style={{
+          width: '300px',
+          height: '300px',
+          background: '#047857',
+          filter: 'blur(100px)',
+          top: '70%',
+          left: '30%',
+        }}
       />
     </div>
   );
